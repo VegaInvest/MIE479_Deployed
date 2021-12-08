db937ca3ce9e67a483ea775bfb6c060ce9ce523b
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-08 09:22:00 -0500

    Update portfolio.py

--- api/src/models/portfolios/portfolio.py
+++ api/src/models/portfolios/portfolio.py
@@ -380,7 +380,7 @@ class Portfolio(object):
         horizon = float(horizon)
         end = int(relativedelta(last_updated, start).years)
         outs = Portfolio.multi_period_backtesting(PortfolioConstants.TICKERS, forecast_window=4, lookback=7, estimation_model=linear_model.SGDRegressor(
-            random_state=42, max_iter=5000), alpha=.1, gamma_trans=.1, gamma_risk=100000, date=Portfolio.to_integer(start), end=end*12, risk_appetite=risk_appetite)
+            random_state=42, max_iter=5000), alpha=.1, gamma_trans=.1, gamma_risk=1000, date=Portfolio.to_integer(start), end=end*12, risk_appetite=risk_appetite)
         curr_weights = outs[0][-1]
         ann_returns = outs[1][1]
         ann_vol = outs[1][2]
