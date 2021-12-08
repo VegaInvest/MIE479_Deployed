109e8d5fc565d86d2d17991cbb42c05df10f3668
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-08 06:13:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,4 @@ app.register_blueprint(stock_blueprint, url_prefix="/stocks")
 
 
 if __name__ == "__main__":
-    p = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=p, host='0.0.0.0')
+    app.run()
