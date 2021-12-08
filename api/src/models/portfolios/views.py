b3906c7833efc992c94f80b8d6c47ae4943ebe5e
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-08 08:36:35 -0500

    aa

--- api/src/models/portfolios/views.py
+++ api/src/models/portfolios/views.py
@@ -31,6 +31,9 @@ from sklearn.preprocessing import StandardScaler
 from scipy.stats.mstats import gmean
 from sklearn import metrics
 from scipy import stats
+from rq import Queue
+from worker import conn
+from app import q
 
 
 portfolio_blueprint = Blueprint("portfolios", __name__)
@@ -130,8 +133,12 @@ def create_portfolio():  # Views form to create portfolio associated with active
         else:
             # start = datetime.datetime.now() actual
             start = datetime.datetime(2018, 1, 2)  # for simulation
-            port = Portfolio.run_backtest(amount_invest=amount_invest, goal=goal, horizon=horizon,
-                                          email=email, risk_appetite=risk_appetite, start=start, last_updated=PortfolioConstants.END_DATE)
+
+            from api.src.models.portfolios.portfolio import run_backtest
+
+            port = q.enqueue_call(func=run_backtest,args=(amount_invest, goal, horizon, email, risk_appetite, start, PortfolioConstants.END_DATE))
+            # port = Portfolio.run_backtest(amount_invest=amount_invest, goal=goal, horizon=horizon,
+            #                               email=email, risk_appetite=risk_appetite, start=start, last_updated=PortfolioConstants.END_DATE)
             port.save_to_mongo()
             # X[1][1] is annualized returns
             #X[1][2] is vol
@@ -215,8 +222,11 @@ def pushParams(email):
             PortfolioConstants.END_DATE, last_updated).months)
 
         if time_diff_update > 1:
-            port = Portfolio.run_backtest(amount_invest=amount_invest, goal=goal, horizon=horizon,
-                                          email=email, risk_appetite=risk_appetite, start=start)
+            from api.src.models.portfolios.portfolio import run_backtest
+
+            port = q.enqueue_call(func=run_backtest,args=(amount_invest, goal, horizon, email, risk_appetite, start, PortfolioConstants.END_DATE))
+            # port = Portfolio.run_backtest(amount_invest=amount_invest, goal=goal, horizon=horizon,
+            #                               email=email, risk_appetite=risk_appetite, start=start)
             port.save_to_mongo()
             port_data = Database.find_one(
                 PortfolioConstants.COLLECTION, {"user_email": email})
