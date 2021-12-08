adf651e9626869f5934bb06e46c2e706722992d6
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-08 18:37:03 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,8 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from api.src.common.database import Database
+from api.src.models.stocks.stock import Stock
+from api.src.models.portfolios.portfolio import Portfolio
+import api.src.models.portfolios.constants as PortfolioConstants
+import api.src.models.stocks.constants as StockConstants
+import datetime
 
 sched = BlockingScheduler()
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17,minute=45)
+@sched.scheduled_job('cron', day_of_week='mon-fri', second=55)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    end_date = datetime.datetime.today()
+    start_date = end_date - datetime.timedelta(days=1)
+    Stock.update_mongo_daily(start_date, end_date, StockConstants.TICKERS)
 
 sched.start()
