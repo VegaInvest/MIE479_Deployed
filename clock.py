01e52aaab6fa4441d00f895a495aa197e5e1ad06
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-08 19:18:55 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -43,43 +43,42 @@ def scheduled_job():
         Stock.push_rawData(PortfolioConstants.START_DATE, end_date)
     Stock.update_mongo_daily(start_date, end_date, PortfolioConstants.TICKERS)
 
-@sched.scheduled_job('interval', days=15)
+@sched.scheduled_job('interval', days=15,next_run_time=datetime.datetime.now())
 def timed_job():
     last_updated = PortfolioConstants.END_DATE
-    start = datetime.datetime(2018, 1, 2)
-    end = int(relativedelta(last_updated, start).years)
-    outs = Portfolio.multi_period_backtesting(PortfolioConstants.TICKERS, forecast_window=4, lookback=7, estimation_model=linear_model.SGDRegressor(
-                random_state=42, max_iter=5000), alpha=.1, gamma_trans=.1, gamma_risk=100000, date=Portfolio.to_integer(start), end=end*12, risk_appetite="low")
-    curr_weights = outs[0][-1]
-    ann_returns = outs[1][1]
-    ann_vol = outs[1][2]
-    sharpe = outs[1][3]
-    port_val = outs[1][-1]
+    if Database.find_one('portfolios',{"risk_appetite":"low", "last_updated" : last_updated})!=None:
+        start = datetime.datetime(2018, 1, 2)
+        end = int(relativedelta(last_updated, start).years)
+        outs = Portfolio.multi_period_backtesting(PortfolioConstants.TICKERS, forecast_window=4, lookback=7, estimation_model=linear_model.SGDRegressor(
+                    random_state=42, max_iter=5000), alpha=.1, gamma_trans=.1, gamma_risk=100000, date=Portfolio.to_integer(start), end=end*12, risk_appetite="low")
+        curr_weights = outs[0][-1]
+        ann_returns = outs[1][1]
+        ann_vol = outs[1][2]
+        sharpe = outs[1][3]
+        port_val = outs[1][-1]
 
-    # convert dates to string
-    dates = outs[1][-2]
-    date_vector = []
-    for date in dates:
-        ts = pd.to_datetime(str(date))
-        date = ts.strftime('%Y-%m-%d')
-        date_vector.append(date)
+        # convert dates to string
+        dates = outs[1][-2]
+        date_vector = []
+        for date in dates:
+            ts = pd.to_datetime(str(date))
+            date = ts.strftime('%Y-%m-%d')
+            date_vector.append(date)
 
-    port = Portfolio(
-        email="sample@sample.com",
-        risk_appetite='low',
-        amount_invest='',
-        goal='',
-        horizon='',
-        curr_weights=curr_weights.tolist(),
-        ann_returns=ann_returns,
-        ann_vol=ann_vol,
-        sharpe=sharpe,
-        port_val=port_val.tolist(),
-        last_updated=last_updated,
-        start=start,
-        date_vector=date_vector
-    )
-    port.save_to_mongo()
-
-
+        port = Portfolio(
+            email="sample@sample.com",
+            risk_appetite='low',
+            amount_invest='',
+            goal='',
+            horizon='',
+            curr_weights=curr_weights.tolist(),
+            ann_returns=ann_returns,
+            ann_vol=ann_vol,
+            sharpe=sharpe,
+            port_val=port_val.tolist(),
+            last_updated=last_updated,
+            start=start,
+            date_vector=date_vector
+        )
+        port.save_to_mongo()
 sched.start()
